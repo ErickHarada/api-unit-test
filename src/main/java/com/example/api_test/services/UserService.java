e871b8c14ee842e7352032866f4f59f54f5fdbb5
Erick <eefeaga2000@gmail.com>
2024-09-03 09:08:44 -0300

    interface user service

--- src/main/java/com/example/api_test/services/UserService.java
+++ src/main/java/com/example/api_test/services/UserService.java
@@ -1,2 +1,8 @@
-package com.example.api_test.services;public interface UserService {
+package com.example.api_test.services;
+
+import com.example.api_test.domain.User;
+
+public interface UserService {
+
+    User findById(Integer id);
 }
