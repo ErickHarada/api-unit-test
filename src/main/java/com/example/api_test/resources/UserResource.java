df07fc710c6c02d14f0fbbc2aafe2c411ad2df5e
Erick <eefeaga2000@gmail.com>
2024-09-03 09:10:41 -0300

    classe user resource implementando findById

--- src/main/java/com/example/api_test/resources/UserResource.java
+++ src/main/java/com/example/api_test/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.example.api_test.resources;
 
 import com.example.api_test.domain.User;
+import com.example.api_test.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(new User());
+        return ResponseEntity.ok(userService.findById(id));
     }
 }
