ec9f01f6143c6f12801718e7d0f189ab1a58b15e
Erick <eefeaga2000@gmail.com>
2024-09-03 09:50:05 -0300

    classe user dto

--- src/main/java/com/example/api_test/domain/dto/UserDTO.java
+++ src/main/java/com/example/api_test/domain/dto/UserDTO.java
@@ -1,2 +1,18 @@
-package com.example.api_test.domain.dto;public class UserDTO {
+package com.example.api_test.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
 }
