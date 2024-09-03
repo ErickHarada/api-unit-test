125a4362e5204142ba4e1b189c30700e220f2373
Erick <eefeaga2000@gmail.com>
2024-09-03 09:09:20 -0300

    classe user service

--- src/main/java/com/example/api_test/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/api_test/services/impl/UserServiceImpl.java
@@ -1,2 +1,22 @@
-package com.example.api_test.services.impl;public class UserServiceImpl {
+package com.example.api_test.services.impl;
+
+import com.example.api_test.domain.User;
+import com.example.api_test.repositories.UserRepository;
+import com.example.api_test.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
 }
