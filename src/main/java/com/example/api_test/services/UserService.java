99d96b49d60d4aaf77955e3361f2fe14bcbf98db
Erick <eefeaga2000@gmail.com>
2024-09-03 10:23:24 -0300

    endpoint create para user

--- src/main/java/com/example/api_test/services/UserService.java
+++ src/main/java/com/example/api_test/services/UserService.java
@@ -1,12 +1,13 @@
 package com.example.api_test.services;
 
 import com.example.api_test.domain.User;
+import com.example.api_test.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/com/example/api_test/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/api_test/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.api_test.services.impl;
 
 import com.example.api_test.domain.User;
+import com.example.api_test.domain.dto.UserDTO;
 import com.example.api_test.repositories.UserRepository;
 import com.example.api_test.services.UserService;
 import com.example.api_test.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private ModelMapper mapper;
     @Autowired
     private UserRepository userRepository;
 
@@ -25,5 +29,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return userRepository.save(mapper.map(obj, User.class));
     }
 }
