f46600b645ba7434c5a006ee1d57d27f368b8ed4
Erick <eefeaga2000@gmail.com>
2024-09-04 08:55:26 -0300

    endpoint delete para user

--- src/main/java/com/example/api_test/resources/UserResource.java
+++ src/main/java/com/example/api_test/resources/UserResource.java
@@ -16,12 +16,13 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserResource {
 
+    public static final String ID = "/{id}";
     @Autowired
     private ModelMapper mapper;
     @Autowired
     private UserService userService;
 
-    @GetMapping("/{id}")
+    @GetMapping(ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         User user = userService.findById(id);
         UserDTO userDTO = mapper.map(user, UserDTO.class);
@@ -47,11 +48,17 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
         obj.setId(id);
         User user = userService.update(obj);
         UserDTO userDTO = mapper.map(user, UserDTO.class);
         return ResponseEntity.ok(userDTO);
+    }
+
+    @DeleteMapping(ID)
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
